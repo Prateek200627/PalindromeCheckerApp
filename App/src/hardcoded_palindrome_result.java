96a8466f03d5e6c96446ac0e5a37d100a73fa469
Prateek200627 <prateekbasu2006@gmail.com>
2026-02-27 10:14:54 +0530

    UC2 Hard code panildrome result

--- App/src/hardcoded_palindrome_result.java
+++ App/src/hardcoded_palindrome_result.java
@@ -4,23 +4,19 @@ public class hardcoded_palindrome_result {
 
             Scanner scanner = new Scanner(System.in);
 
-            // Take user input
             System.out.print("Input Text: ");
             String input = scanner.nextLine();
 
             boolean isPalindrome = true;
 
-            // Loop only till half of the string length
             for (int i = 0; i < input.length() / 2; i++) {
 
-                // Compare characters from both ends
                 if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
                     isPalindrome = false;
                     break;
                 }
             }
 
-            // Display result
             if (isPalindrome) {
                 System.out.println("Is it a palindrome? : True");
             } else {
